02a66bc20ba88adc15cb2f0d99148c77421ef750
Ehsan U <lancerf562@gmail.com>
2023-12-09 11:03:07 +0500

    update

--- src/http_requests.py
+++ src/http_requests.py
@@ -1,13 +1,16 @@
-from base64 import b64decode
-from typing import Dict, Optional
 from aiolimiter import AsyncLimiter
-from httpx import Response
+from base64 import b64decode
+import os
+from tenacity import AsyncRetrying, retry, stop_after_attempt, wait_random_exponential
 import httpx
-from tenacity import AsyncRetrying, retry, stop_after_attempt, wait_exponential
+from typing import Dict, Optional
+from httpx import Response
 
 
-from src.logger import logger
 from src.http_response import ResponseWrapper
+from src.logger import logger
+
+
 
 
 
@@ -41,6 +44,7 @@ class BaseRequest:
             json: Dict = None,
             verify: bool = True,
             proxies: Dict = None,
+            timeout: int = None
         ):
         self.url = url
         self.method = method
@@ -51,6 +55,7 @@ class BaseRequest:
         self.json = json
         self.verify = verify
         self.proxies = proxies
+        self.timeout = timeout or self.TIMEOUT
 
 
     def __repr__(self):
@@ -67,9 +72,10 @@ class BaseRequest:
         Returns:
             A response object indicating the failure.
         """
-        logger.error(f"All retries failed to {self.url}: {e}")
+        logger.error(f"All retries failed to {self.url}: {e}", exc_info=True)
         return Response(status_code=420, url=self.url, text=str(e))
-    
+
+
 
 
 class Request(BaseRequest):
@@ -81,7 +87,7 @@ class Request(BaseRequest):
         process_request: Processes the HTTP request and returns a ResponseWrapper object.
     """
 
-    @retry(stop=stop_after_attempt(BaseRequest.RETRIES), wait=wait_exponential(multiplier=1, min=4, max=10), reraise=True)
+    @retry(stop=stop_after_attempt(BaseRequest.RETRIES), wait=wait_random_exponential(multiplier=1, min=4, max=10), reraise=True)
     def send(self) -> Response:
         """
         Sends the HTTP request and returns the response.
@@ -90,23 +96,18 @@ class Request(BaseRequest):
             The HTTP response.
         """
 
-        try:
-            with httpx.Client(verify=self.verify, timeout=self.TIMEOUT, proxies=self.proxies) as client:
-                response = client.request(
-                    url=self.url, 
-                    method=self.method, 
-                    headers=self.headers, 
-                    cookies=self.cookies, 
-                    params=self.params, 
-                    data=self.data, 
-                    json=self.json, 
-                )
-                response.raise_for_status()
-                return response
-            
-        except Exception as e:
-            logger.error(f"Request to {self.url} failed [Retrying]: {e}")
-            raise e
+        with httpx.Client(verify=self.verify, timeout=self.timeout, proxies=self.proxies) as client:
+            response = client.request(
+                url=self.url, 
+                method=self.method, 
+                headers=self.headers, 
+                cookies=self.cookies, 
+                params=self.params, 
+                data=self.data, 
+                json=self.json, 
+            )
+            response.raise_for_status()
+            return response
 
 
     def process_request(self) -> Optional[ResponseWrapper]:
@@ -119,13 +120,12 @@ class Request(BaseRequest):
 
         try:
             response = self.send()
-
         except Exception as e:
-            logger.error(e)
+            logger.error(e, exc_info=True)
             return None
-        
         else:
             return ResponseWrapper(response)
+
 
 
 
@@ -152,8 +152,8 @@ class AsyncRequest(BaseRequest):
             Response: The response received from the server.
         """
 
-        async with httpx.AsyncClient(verify=self.verify, timeout=self.TIMEOUT, proxies=self.proxies) as client:
-            async for attempt in AsyncRetrying(stop=stop_after_attempt(self.RETRIES), wait=wait_exponential(multiplier=1, min=4, max=10), reraise=True):
+        async with httpx.AsyncClient(verify=self.verify, timeout=self.timeout, proxies=self.proxies) as client:
+            async for attempt in AsyncRetrying(stop=stop_after_attempt(self.RETRIES), wait=wait_random_exponential(multiplier=1, min=4, max=10), reraise=True):
                 with attempt:
                     async with self.RATE_LIMIT:
                         response = await client.request(
@@ -181,60 +181,43 @@ class AsyncRequest(BaseRequest):
         try:
             response = await self.send()
         except Exception as e:
-            logger.error(e)
+            logger.error(e, exc_info=True)
             return None
         else:
             return ResponseWrapper(response)
-        
 
 
-class ZYTE_REQUEST(BaseRequest):
+
+
+class Zyte_Request(Request):
     """
     Represents a request to the Zyte API.
 
     Args:
         zyte_api_key (str): The API key for accessing the Zyte API.
         browser (bool, optional): Whether to use browser rendering for the request. Defaults to False.
-        async_mode (bool, optional): Whether to send the request asynchronously. Defaults to False.
 
     Attributes:
         ZYTE_ENDPOINT (str): The endpoint URL for the Zyte API.
 
     Methods:
-        async_zyte_send: Sends the request asynchronously and returns the response.
-        sync_zyte_send: Sends the request synchronously and returns the response.
+        send: Sends the HTTP request and returns the response.
         prepare_payload: Prepares the payload for the request.
-        execute_request: Executes the request and returns the response.
         process_request: Processes the request and returns a wrapped response.
     """
 
     ZYTE_ENDPOINT: str = "https://api.zyte.com/v1/extract"
-    RATE_LIMIT: AsyncLimiter = AsyncLimiter(100, 60)
 
 
-    def __init__(self, zyte_api_key: str, browser: bool = False, async_mode: bool = False, *args, **kwargs):
+    def __init__(self, zyte_api_key: str = None, browser: bool = False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.zyte_api_key = zyte_api_key
+        self.zyte_api_key = zyte_api_key or os.getenv("ZYTE_API_KEY")
         self.browser = browser
-        self.async_mode = async_mode
 
 
-    async def async_zyte_send(self) -> Response:
-        """
-        Sends the request asynchronously and returns the response.
-
-        Returns:
-            The response object.
-        """
-
-        json_payload = self.prepare_payload()
-
-        async with httpx.AsyncClient(verify=self.verify, timeout=self.TIMEOUT) as client:
-            return await self.execute_request(client, json_payload)
-
-
-    def sync_zyte_send(self) -> Response:
+    @retry(stop=stop_after_attempt(BaseRequest.RETRIES), wait=wait_random_exponential(multiplier=1, min=4, max=10), reraise=True)
+    def send(self) -> Response:
         """
         Sends the request synchronously and returns the response.
 
@@ -244,8 +227,96 @@ class ZYTE_REQUEST(BaseRequest):
 
         json_payload = self.prepare_payload()
         
-        with httpx.Client(verify=self.verify, timeout=self.TIMEOUT) as client:
-            return self.execute_request(client, json_payload)
+        with httpx.Client(verify=self.verify, timeout=self.timeout) as client:
+            response = client.post(self.ZYTE_ENDPOINT, auth=(self.zyte_api_key, ""), json=json_payload)
+            response.raise_for_status()
+
+            if self.browser:
+                http_response_body = response.json()["browserHtml"]
+            else:
+                http_response_body = b64decode(response.json()["httpResponseBody"]).decode()
+
+            return Response(
+                status_code=response.status_code, 
+                request=client.build_request(url=self.url, method=self.method), 
+                text=http_response_body
+            )
+        
+
+    def prepare_payload(self) -> dict:
+        """
+        Prepares the payload for the request.
+
+        Returns:
+            The prepared payload.
+        """
+
+        if self.browser:
+            return {"url": self.url, "browserHtml": True}
+        else:
+            return {
+                "url": self.url,
+                "httpResponseBody": True,
+                "httpRequestMethod": self.method
+            }
+        
+
+
+class Zyte_AsyncRequest(AsyncRequest):
+    """
+    Represents a request to the Zyte API.
+
+    Args:
+        zyte_api_key (str): The API key for accessing the Zyte API.
+        browser (bool, optional): Whether to use browser rendering for the request. Defaults to False.
+
+    Attributes:
+        ZYTE_ENDPOINT (str): The endpoint URL for the Zyte API.
+        RATE_LIMIT: The rate limiter for the request.
+
+    Methods:
+        send: Sends the request asynchronously and returns the response.
+        prepare_payload: Prepares the payload for the request.
+        process_request: Processes the request and returns a wrapped response.
+    """
+
+    ZYTE_ENDPOINT: str = "https://api.zyte.com/v1/extract"
+
+
+    def __init__(self, zyte_api_key: str = None, browser: bool = False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.zyte_api_key = zyte_api_key or os.getenv("ZYTE_API_KEY")
+        self.browser = browser
+
+
+    async def send(self) -> Response:
+        """
+        Sends the request asynchronously and returns the response.
+
+        Returns:
+            The response object.
+        """
+
+        json_payload = self.prepare_payload()
+
+        async with httpx.AsyncClient(verify=self.verify, timeout=self.timeout) as client:
+            async for attempt in AsyncRetrying(stop=stop_after_attempt(self.RETRIES), wait=wait_random_exponential(multiplier=1, min=4, max=10), reraise=True):
+                with attempt:
+                    async with self.RATE_LIMIT:
+                        response = await client.post(self.ZYTE_ENDPOINT, auth=(self.zyte_api_key, ""), json=json_payload)
+                        response.raise_for_status()
+
+                        if self.browser:
+                            http_response_body = response.json()["browserHtml"]
+                        else:
+                            http_response_body = b64decode(response.json()["httpResponseBody"]).decode()
+
+                        return Response(
+                            status_code=response.status_code, 
+                            request=client.build_request(url=self.url, method=self.method), 
+                            text=http_response_body
+                        )
 
 
     def prepare_payload(self) -> dict:
@@ -265,54 +336,3 @@ class ZYTE_REQUEST(BaseRequest):
                 "httpRequestMethod": self.method
             }
 
-
-    async def execute_request(self, client, json_payload) -> Response:
-        """
-        Executes the request and returns the response.
-
-        Args:
-            client: The HTTP client.
-            json_payload: The JSON payload for the request.
-
-        Returns:
-            The response object.
-        """
-
-        async for attempt in AsyncRetrying(stop=stop_after_attempt(self.RETRIES), wait=wait_exponential(multiplier=1, min=4, max=10), reraise=True):
-            with attempt:
-                async with self.RATE_LIMIT:
-                    response = await client.post(self.ZYTE_ENDPOINT, auth=(self.zyte_api_key, ""), json=json_payload)
-                    response.raise_for_status()
-
-                    if self.browser:
-                        http_response_body = response.json()["browserHtml"]
-                    else:
-                        http_response_body = b64decode(response.json()["httpResponseBody"]).decode()
-
-                    return Response(
-                        status_code=response.status_code, 
-                        request=client.build_request(url=self.url, method=self.method), 
-                        text=http_response_body
-                    )
-
-
-    async def process_request(self) -> Optional[ResponseWrapper]:
-        """
-        Processes the request and returns a wrapped response.
-
-        Returns:
-            The wrapped response object, or None if an error occurred.
-        """
-
-        try:
-            if self.async_mode:
-                response = await self.async_zyte_send()
-            else:
-                response = self.sync_zyte_send()  
-
-        except Exception as e:
-            logger.error(e)
-            return None
-        
-        else:
-            return ResponseWrapper(response)
